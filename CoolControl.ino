e6f27d59fa55afa309022cee632f721a3afffecd
Alik Tabunov <aliktab@gmail.com>
2021-10-21 00:11:07 +0200

    no message

--- CoolControl.ino
+++ CoolControl.ino
@@ -32,10 +32,11 @@ const uint8_t PIN_FAN[] = { 11, 12 };
 // Time constants
 
 const int64_t CYCLE_PAUSE  = (int64_t)1000;   // Duration of signal calculation cycle.
-const int64_t CHECK_LENGTH = (int64_t)10;      // Number of cycles per check.
+const int64_t CHECK_LENGTH = (int64_t)10;     // Number of cycles per check.
+const int64_t T_LOG_TIME   = (int64_t)600000; // How often log temperature.
 
 const int64_t MAX_ON_TIME  = (int64_t)30 * 60 * 1000;      // Max time FAN is on.
-const int64_t MIN_ON_TIME  = (int64_t)2  * 60 * 1000;      // Max time FAN is on.
+const int64_t MIN_ON_TIME  = (int64_t)2  * 60 * 1000;      // Min time FAN is on.
 
 const int64_t MAX_OFF_TIME = (int64_t)6 * 60 * 60 * 1000;  // Max time FAN is off.
 
@@ -46,9 +47,9 @@ const uint8_t FAN_STATE_OFF  = 0;
 const uint8_t FAN_STATE_LOW  = 1;
 const uint8_t FAN_STATE_HIGH = 2;
 
-const float TEMPER_OFF   = 38.0;   // Temperature when fans should be switched off.
-const float TEMPER_ON    = 42.8;   // Temperature when fans should be switched on.
-const float TEMPER_NORM  = 40.0;   // Normal temperature when we don't need to switch fan at all.
+const float TEMPER_OFF   = 30.0;   // Temperature when fans should be switched off.
+const float TEMPER_NORM  = 34.0;   // Normal temperature when we don't need to switch fan at all.
+const float TEMPER_ON    = 36.0;   // Temperature when fans should be switched on.
 
 
 // Global variables
@@ -57,9 +58,10 @@ MCP9808 t_sensor = MCP9808(0x18);
 
 int64_t start_time = 0;
 int64_t mode_time  = 0;
+int64_t save_time  = 0;
+int64_t t_log_time = 0;
 
 bool fan_switched_on = false;
-
 
 // Initialization
 
@@ -87,7 +89,9 @@ void setup()
 
   // Initialize time system.
   start_time = millis();
-  mode_time = 0;
+  mode_time  = 0;
+  save_time  = 0;
+  t_log_time = 0;
 
   // Init fan flag
   fan_switched_on = false;
@@ -111,20 +115,21 @@ void loop()
   float temperature = 0.0;
   for (int idx = 0; idx < CHECK_LENGTH; idx++)
   {
-    temperature += t_sensor.read_temp_C() / (float)CHECK_LENGTH;
+    temperature += t_sensor.read_temp_C();
     delay(CYCLE_PAUSE);
   }
+  temperature /= (float)CHECK_LENGTH;
 
   // Check temperature and manage fans
-  // if (!fan_switched_on && (temperature > TEMPER_ON ||
-  //                          temperature > TEMPER_NORM && mode_time > MAX_OFF_TIME))
-  if (!fan_switched_on && (temperature > TEMPER_ON))
+  if (!fan_switched_on && (temperature > TEMPER_ON ||
+                           temperature > TEMPER_NORM && mode_time > MAX_OFF_TIME))
   {
     // Switch fan on.
     for (int idx = 0; idx < sizeof(PIN_FAN) / sizeof(uint8_t); idx++)
       digitalWrite(PIN_FAN[idx], HIGH);
 
     // Reset mode time counter.
+    save_time = mode_time;
     mode_time = 0;
 
     // Set fan mode flag.
@@ -139,6 +144,7 @@ void loop()
       digitalWrite(PIN_FAN[idx], LOW);
 
     // Reset mode time counter.
+    save_time = mode_time;
     mode_time = 0;
 
     // Set fan mode flag.
@@ -146,17 +152,34 @@ void loop()
   }
 
   // Serrialize current data.
-  char log_str[128];
-  sprintf(log_str, "  %s time is %li:%02li:%02li, Temperature: ",
-      fan_switched_on ? "ON " : "OFF",
-      (long)((mode_time / 3600000)     ),
-      (long)((mode_time / 60000  ) % 60),
-      (long)((mode_time / 1000   ) % 60)
-    );
-  Serial.print(log_str);
-  Serial.println(temperature);
+  if (0 == mode_time) 
+  {
+    t_log_time = millis() - T_LOG_TIME;
+    
+    char log_str[256];
+    sprintf(log_str, "  %s (%li:%02li:%02li) -> %s",
+        fan_switched_on ? "OFF" : "ON ",
+        (long)((save_time / 3600000)     ),
+        (long)((save_time / 60000  ) % 60),
+        (long)((save_time / 1000   ) % 60),
+        fan_switched_on ? "ON " : "OFF"
+      );
+    Serial.println("");
+    Serial.println("");
+    Serial.print(log_str);
+  }
+  else
+  {
+    if (millis() - t_log_time > T_LOG_TIME)
+    {
+      t_log_time = millis();
+      
+      Serial.println("");
+      Serial.print("    ");
+      Serial.print(temperature);
+      Serial.print(" ");
+    }
+    else
+      Serial.print(".");
+  }
 }
-
-
-
-
